a8d4019ba68cc14ac2d4fadf82c21b3089d22e34
Trung Kien <kien123456k@gmail.com>
2020-07-17 00:39:51 +0700

    upload 3 exercises

---
+++ Survey form/Survey form.js
@@ -0,0 +1,5 @@
+var submit = document.getElementsByTagName("button");
+submit[0].addEventListener("click", thanks);
+function thanks() {
+  alert("Thanks for your feedback! Hope see you again!");
+}
